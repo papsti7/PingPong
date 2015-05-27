57d7438b57be3f65b6b1749ddb32aadc94cd902a
Stefan Papst <stefan.papst@gmx.at>
2015-05-27 19:46:02 +0200

    -put keyboard handling out of pollevent loop to get smooth movement

--- src/main.cpp
+++ src/main.cpp
@@ -45,31 +45,25 @@ int main()
       case sf::Event::Closed: 
         window.close();
         break;
-      case sf::Event::KeyPressed:
-        switch (event.key.code)
-        {
-          case sf::Keyboard::W:
-            sprite.move(0.f,-5.f);
-            //std::cout << "notized key W" << std::endl;
-            break;
-          case sf::Keyboard::S:
-            sprite.move(0.f, 5.f);
-            //std::cout << "notized key S" << std::endl;
-            break;
-          case sf::Keyboard::A:
-            sprite.move(-5.f, 0.f);
-            //std::cout << "notized key A" << std::endl;
-            break;
-          case sf::Keyboard::D:
-            sprite.move(5.f, 0.f);
-            //std::cout << "notized key D" << std::endl;
-            break;
-        }
-        break;
-
     }
     
   }
+   //smooth movement
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+      sprite.move(0.f, -5.f);
+      //std::cout << "notized key W" << std::endl;
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+      sprite.move(0.f, 5.f);
+        //std::cout << "notized key S" << std::endl;
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+      sprite.move(-5.f, 0.f);
+      //std::cout << "notized key A" << std::endl;
+    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+      sprite.move(5.f, 0.f);
+      //std::cout << "notized key D" << std::endl;
+     
+    
+  
 
   window.clear();
   window.draw(background);
