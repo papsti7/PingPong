becbdada25568795e2720eaed099f4b46d8a46ed
Stefan Papst <stefan.papst@gmx.at>
2015-05-27 19:28:13 +0200

    -added smiley texture and put in sf::sprite -added event handling for keyboard codes "W,A,S,D"

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,74 @@
 //------------------------------------------------------------------------------                                                                                
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
+  //Init the win
   sf::RenderWindow window(sf::VideoMode(600, 600), "SFML PingPong");
+  window.setVerticalSyncEnabled(true);
+  //create background with color "white
   sf::RectangleShape background(sf::Vector2f(600.f, 600.f));
   background.setFillColor(sf::Color::White);
 
-  sf::RectangleShape shape(sf::Vector2f(20.f,20.f));
-  shape.setPosition(290.f,290.f);
-  sf::Color Color(sf::Color::Red);
-  shape.setFillColor(Color);
-  shape.setOutlineColor(sf::Color::Black);
-  shape.setOutlineThickness(2.f);
-  
+  //load texture
+  sf::Texture smiley;
+  if (!smiley.loadFromFile("smiley.png"))
+  {
+    std::cout << "Loading File not possible!" << std::endl;
+    std::cin.get();
+    return 1;
+  }
+  //put texture in sprite
+  smiley.setSmooth(true);
+  sf::Sprite sprite;
+  sprite.setTexture(smiley);
+  sprite.setPosition(275.f, 275.f);
+  sprite.setScale(.5f,.5f);
 
+  //while running loop
   while (window.isOpen())
   {
 
-    sf::Event event;
-    while (window.pollEvent(event))
+  sf::Event event;
+  while (window.pollEvent(event))
+  {
+    switch (event.type)
     {
-        if (event.type == sf::Event::Closed)
+      case sf::Event::Closed: 
         window.close();
-    }
+        break;
+      case sf::Event::KeyPressed:
+        switch (event.key.code)
+        {
+          case sf::Keyboard::W:
+            sprite.move(0.f,-5.f);
+            //std::cout << "notized key W" << std::endl;
+            break;
+          case sf::Keyboard::S:
+            sprite.move(0.f, 5.f);
+            //std::cout << "notized key S" << std::endl;
+            break;
+          case sf::Keyboard::A:
+            sprite.move(-5.f, 0.f);
+            //std::cout << "notized key A" << std::endl;
+            break;
+          case sf::Keyboard::D:
+            sprite.move(5.f, 0.f);
+            //std::cout << "notized key D" << std::endl;
+            break;
+        }
+        break;
 
-    window.clear();
-    window.draw(background);
-    window.draw(shape);
-    window.display();
+    }
+    
+  }
+
+  window.clear();
+  window.draw(background);
+  window.draw(sprite);
+  window.display();
   }
   return 0;
 }
