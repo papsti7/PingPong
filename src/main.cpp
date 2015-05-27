d8ab9805f153892da8b8975a68e7f27e9fec101b
Stefan Papst <stefan.papst@gmx.at>
2015-05-27 18:09:42 +0200

    -some testing with rects and circles

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,36 @@
 // Softwareengeneering and Businessmanagement @ TU GRAZ
 //------------------------------------------------------------------------------                                                                                
 
-                                                                                
-
 #include <SFML/Graphics.hpp>
+
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-  sf::CircleShape shape;
-  shape.setRadius(40.f);
-  shape.setPosition(100.f, 100.f);
-  shape.setFillColor(sf::Color::Cyan);
+  sf::RenderWindow window(sf::VideoMode(600, 600), "SFML PingPong");
+  sf::RectangleShape background(sf::Vector2f(600.f, 600.f));
+  background.setFillColor(sf::Color::White);
 
-  while (window.isOpen()){
+  sf::RectangleShape shape(sf::Vector2f(20.f,20.f));
+  shape.setPosition(290.f,290.f);
+  sf::Color Color(sf::Color::Red);
+  shape.setFillColor(Color);
+  shape.setOutlineColor(sf::Color::Black);
+  shape.setOutlineThickness(2.f);
+  
+
+  while (window.isOpen())
+  {
 
     sf::Event event;
-    while (window.pollEvent(event)){
-
-      if (event.type == sf::Event::Closed)
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
         window.close();
     }
 
     window.clear();
+    window.draw(background);
     window.draw(shape);
     window.display();
   }
-
+  return 0;
 }
